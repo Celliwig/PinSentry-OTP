c905feb33b0244c3d0400f5bfcc1e66c33b7c9cc
Celliwig <celliwig@nym.hush.com>
2021-08-01 18:11:39 +0000

    Update KeySlot. Added keysize variable, added class methods.

--- pstotp/KeySlot.java
+++ pstotp/KeySlot.java
@@ -11,9 +11,31 @@ import javacard.framework.Util;
 public class KeySlot {
 	public static final short MAX_KEY_SIZE_BYTES = (short) 64;
 
-	public byte[] key = null;
+	private byte[] key = null;
+	private short keysize;
 
 	public KeySlot() {
 		key = new byte[MAX_KEY_SIZE_BYTES];
+		keysize = (short) 0;
+	}
+
+	public byte[] getKey() {
+		return key;
+	}
+
+	public short getKeySize() {
+		return keysize;
+	}
+
+	public boolean update(byte[] keyData, short keyOffset, short keySize) {
+		// Check if key data is larger than slot size
+		if (keySize >= KeySlot.MAX_KEY_SIZE_BYTES) return false;
+
+		// Copy key data to slot
+		for (short i = 0; i < KeySlot.MAX_KEY_SIZE_BYTES; i++) {
+			key[i] = keyData[(short) (i + keyOffset)];
+		}
+
+		return true;
 	}
 }
