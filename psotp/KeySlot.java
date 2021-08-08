a618f3844007681e974349273a2010169105a32f
Celliwig <celliwig@nym.hush.com>
2021-08-08 21:26:18 +0000

    Optimised KeySlot.

--- psotp/KeySlot.java
+++ psotp/KeySlot.java
@@ -6,6 +6,7 @@ package psotp;
 
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
 import javacard.framework.Util;
 
 public class KeySlot {
@@ -14,12 +15,12 @@ public class KeySlot {
 
 	private byte[] slotKey = null;
 	private short slotKeySize;
-	private short[] slotCounter = null;
+	private byte[] slotCounter = null;
 
 	public KeySlot() {
 		slotKey = new byte[MAX_KEY_SIZE_BYTES];
 		slotKeySize = (short) 0;
-		slotCounter = new short[COUNTER_SIZE_BYTES];
+		slotCounter = new byte[COUNTER_SIZE_BYTES];
 	}
 
 	// Byte array containg the key
@@ -38,48 +39,133 @@ public class KeySlot {
 	// And increment counter
 	public short getCounter(byte[] out, short outOffset, short outLength) {
 		byte i;
+		boolean counterCarry;
+		short counterDigit;
 
 		// Check size of output buffer
 		if (outLength <= 0) ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
 		if (outLength > COUNTER_SIZE_BYTES) outLength = COUNTER_SIZE_BYTES;
 
 		// Copy counter to buffer
-		for (i = 0; i < outLength; i++) {
-			out[(short) (outOffset + i)] = (byte) (slotCounter[i] & 0x00ff);
-		}
+		Util.arrayCopyNonAtomic(slotCounter, (short) 0, out, outOffset, outLength);
 
-		// Increment counter
-		slotCounter[7]++;
-		if (slotCounter[7] > 0x00ff) {
-			slotCounter[7] = 0x0000;
-			slotCounter[6]++;
+		// Clear carry
+		counterCarry = false;
+		// Increment LSB
+		counterDigit = (short) (slotCounter[7] & 0x00ff);
+		counterDigit++;
+		// Handle byte rollover
+		if (counterDigit > 0x00ff) {
+			counterDigit = 0x0000;
+			counterCarry = true;
 		}
-		if (slotCounter[6] > 0x00ff) {
-			slotCounter[6] = 0x0000;
-			slotCounter[5]++;
-		}
-		if (slotCounter[5] > 0x00ff) {
-			slotCounter[5] = 0x0000;
-			slotCounter[4]++;
-		}
-		if (slotCounter[4] > 0x00ff) {
-			slotCounter[4] = 0x0000;
-			slotCounter[3]++;
-		}
-		if (slotCounter[3] > 0x00ff) {
-			slotCounter[3] = 0x0000;
-			slotCounter[2]++;
-		}
-		if (slotCounter[2] > 0x00ff) {
-			slotCounter[2] = 0x0000;
-			slotCounter[1]++;
-		}
-		if (slotCounter[1] > 0x00ff) {
-			slotCounter[1] = 0x0000;
-			slotCounter[0]++;
-		}
-		if (slotCounter[0] > 0x00ff) {
-			slotCounter[0] = 0x0000;
+		// Write byte back
+		slotCounter[7] = (byte) (counterDigit & 0x00ff);
+		// Handle carry
+		if (counterCarry) {
+			// Clear carry
+			counterCarry = false;
+			// Increment LSB + 1
+			counterDigit = (short) (slotCounter[6] & 0x00ff);
+			counterDigit++;
+			// Handle byte rollover
+			if (counterDigit > 0x00ff) {
+				counterDigit = 0x0000;
+				counterCarry = true;
+			}
+			// Write byte back
+			slotCounter[6] = (byte) (counterDigit & 0x00ff);
+			// Handle carry
+			if (counterCarry) {
+				// Clear carry
+				counterCarry = false;
+				// Increment LSB + 2
+				counterDigit = (short) (slotCounter[5] & 0x00ff);
+				counterDigit++;
+				// Handle byte rollover
+				if (counterDigit > 0x00ff) {
+					counterDigit = 0x0000;
+					counterCarry = true;
+				}
+				// Write byte back
+				slotCounter[5] = (byte) (counterDigit & 0x00ff);
+				// Handle carry
+				if (counterCarry) {
+					// Clear carry
+					counterCarry = false;
+					// Increment LSB + 3
+					counterDigit = (short) (slotCounter[4] & 0x00ff);
+					counterDigit++;
+					// Handle byte rollover
+					if (counterDigit > 0x00ff) {
+						counterDigit = 0x0000;
+						counterCarry = true;
+					}
+					// Write byte back
+					slotCounter[4] = (byte) (counterDigit & 0x00ff);
+					// Handle carry
+					if (counterCarry) {
+						// Clear carry
+						counterCarry = false;
+						// Increment LSB + 4
+						counterDigit = (short) (slotCounter[3] & 0x00ff);
+						counterDigit++;
+						// Handle byte rollover
+						if (counterDigit > 0x00ff) {
+							counterDigit = 0x0000;
+							counterCarry = true;
+						}
+						// Write byte back
+						slotCounter[3] = (byte) (counterDigit & 0x00ff);
+						// Handle carry
+						if (counterCarry) {
+							// Clear carry
+							counterCarry = false;
+							// Increment LSB + 5
+							counterDigit = (short) (slotCounter[2] & 0x00ff);
+							counterDigit++;
+							// Handle byte rollover
+							if (counterDigit > 0x00ff) {
+								counterDigit = 0x0000;
+								counterCarry = true;
+							}
+							// Write byte back
+							slotCounter[2] = (byte) (counterDigit & 0x00ff);
+							// Handle carry
+							if (counterCarry) {
+								// Clear carry
+								counterCarry = false;
+								// Increment LSB + 6
+								counterDigit = (short) (slotCounter[1] & 0x00ff);
+								counterDigit++;
+								// Handle byte rollover
+								if (counterDigit > 0x00ff) {
+									counterDigit = 0x0000;
+									counterCarry = true;
+								}
+								// Write byte back
+								slotCounter[1] = (byte) (counterDigit & 0x00ff);
+								// Handle carry
+								if (counterCarry) {
+									// Clear carry
+									counterCarry = false;
+									// Increment LSB + 7
+									counterDigit = (short) (slotCounter[0] & 0x00ff);
+									counterDigit++;
+									// Handle byte rollover
+									if (counterDigit > 0x00ff) {
+										counterDigit = 0x0000;
+										counterCarry = true;
+									}
+									// Write byte back
+									slotCounter[0] = (byte) (counterDigit & 0x00ff);
+									// Ignore carry
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 
 		return COUNTER_SIZE_BYTES;
@@ -92,20 +178,18 @@ public class KeySlot {
 		if (keySize >= KeySlot.MAX_KEY_SIZE_BYTES) return false;
 
 		// Copy key data to slot
-		for (short i = 0; i < keySize; i++) {
-			slotKey[i] = keyData[(short) (i + keyOffset)];
-		}
+		Util.arrayCopyNonAtomic(keyData, keyOffset, slotKey, (short) 0, keySize);
 		slotKeySize = keySize;
 
 		// Reset slot counter
-		slotCounter[0] = 0x0000;
-		slotCounter[1] = 0x0000;
-		slotCounter[2] = 0x0000;
-		slotCounter[3] = 0x0000;
-		slotCounter[4] = 0x0000;
-		slotCounter[5] = 0x0000;
-		slotCounter[6] = 0x0000;
-		slotCounter[7] = 0x0000;
+		slotCounter[0] = 0x00;
+		slotCounter[1] = 0x00;
+		slotCounter[2] = 0x00;
+		slotCounter[3] = 0x00;
+		slotCounter[4] = 0x00;
+		slotCounter[5] = 0x00;
+		slotCounter[6] = 0x00;
+		slotCounter[7] = 0x00;
 
 		return true;
 	}
